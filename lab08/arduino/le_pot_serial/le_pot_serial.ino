8a3f8fcd441174d5a3a7fbf4ca734e28e3ffe1f7
Lucas <lucas.gauvain@etu.sorbonne-universite.fr>
2024-12-09 20:30:36 -0300

    implement intervalo

--- lab08/arduino/le_pot_serial/le_pot_serial.ino
+++ lab08/arduino/le_pot_serial/le_pot_serial.ino
@@ -1,10 +1,14 @@
 const int analogInPin = A0;            // o potenciômetro esta ligado ao pino A0
 int iniciaColeta = 0;
+int iniciaIntervalo = 0;
 char charRecebido;                     // cria uma variavel para armazenar o caractere recebido
+long anterior, agora, intervalo, ultimaMedicao;
+int meu_delay = 100;
 
 void setup(){
    // Configura a serial: baud rate de 115200, 8-bit, sem paridade, 1 stop bit
    Serial.begin(115200, SERIAL_8N1);
+   anterior = millis();
 }
 
 void loop(){
@@ -18,6 +22,23 @@ void loop(){
           case 'p':                    // para a coleta
              iniciaColeta = 0;
              break;
+
+          case 'c':
+             Serial.write(intervalo & 0xFF);
+             Serial.write(intervalo >> 8 & 0xFF);
+
+             break;
+             
+          case 'd':
+            // aaa = aaa + 1;
+            meu_delay+=5;
+            break;
+             
+          case 'e':
+            // aaa = aaa - 1;            
+            meu_delay-=5;
+            break;
+                
              
           default:                     // outro comando, ignora...
              break;
@@ -26,7 +47,12 @@ void loop(){
    if ( iniciaColeta == 1 ){
        int valor = analogRead(analogInPin); // le valor no pino A0 usando conversor ADC de 10-bit
        Serial.write(valor & 0xFF);          // envia byte menos significativo
-       Serial.write(valor >> 8);            // envia byte mais significativo
+       Serial.write(valor >> 8);            // envia byte mais significativ
+       agora = millis();
+       intervalo = agora-anterior;
+       anterior = agora;
    }
-   delay(100);                          // aguarda 100ms
+   delay(meu_delay);                          // aguarda 100ms
+   
+
 }
